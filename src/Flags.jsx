463b37af2e4a2d5b6096a96f1d75d8c6c7f74405
Umrbek <umrbektursunboyev32@gmail.com>
2026-05-02 17:57:06 +0500

    wow

--- src/Flags.jsx
+++ src/Flags.jsx
@@ -4,6 +4,8 @@ const CountryList = () => {
     const [countries, setCountries] = useState([])
     const [loading, setLoading] = useState([true])
     const [error, setError] = useState([null])
+    const [region, setRegion] = useState('')
+    const [search, setSearch] = useState('')
 
     const API_URL = 'https://restcountries.com/v3.1/all?fields=name,capital,currencies,continents,borders,flags,languages,population,region'
 
@@ -34,12 +36,26 @@ const CountryList = () => {
         getCountries()
     }, [])
 
+    const filterRegion = countries.filter((country) =>{
+        const selectRegion = region === '' || country.region === region
+        return selectRegion;
+    })
+
     if (loading) return <h2 style={{textAlign:'center'}}>Dunyo Xaritasi Yuklanmoqda   ffffff...</h2>
     // if (error) return <h2 style={{color:'red', textAlign:'center'}}>Xato: yana {error}</h2>
 
     return(
         <div style={{padding:'20px', fontFamily:'Arial'}}>
             <h1 style={{textAlign:'center'}}>Davlatlar Ma`lumotnomasi</h1>
+
+            <select name="" id="" onChange={(e) => setRegion(e.target.value)}>
+                <option value="">Barchasi</option>
+                <option value="Africa">Africa</option>
+                <option value="Americas"> Americas</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+            </select>
 
             <div
             style={{
@@ -48,7 +64,7 @@ const CountryList = () => {
                 gap:'20px',
                 marginTop:'20px'
             }}>
-                {countries.map((country, index) => (
+                {filterRegion.map((country, index) => (
                     <div key={index} style={{
                         border:'1px solid #ddd',
                         borderRadius:'10px',
@@ -62,7 +78,7 @@ const CountryList = () => {
                         style={{width:'100%', height:'150px', objectFit:'cover', borderRadius:'5px'}} />
                         <h3 style={{margin:'10px 0'}}>{country.name.common}</h3>
                         <p><strong>Poytaxti:</strong> {country.capital ? country.capital[0] : 'Mavjud emas'}</p>
-                        <p><strong>Poytaxti:</strong> {country.region}</p>
+                        <p><strong>Mintaqasi:</strong> {country.region}</p>
                         <p><strong>Aholi:</strong> {country.population.toLocaleString()}</p>
                     </div>
                 ))}
